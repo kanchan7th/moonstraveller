a4ef49e8a704c0779b64d0da6daee5c42767d7c6
kanchan <kanchanbhuju@gmail.com>
2020-08-04 17:59:46 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,688 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="img/favicon.png" type="image/png">
+	<title>Moon Traveller</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="vendors/linericon/style.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<style>
+#nav-bar 
+{
+/*	overflow:hidden;*/
+	position: sticky;
+	top:0;
+	z-index:10;
+}
+.navbar{
+	background:linear-gradient(to right,#94618E, #764d71);
+	padding:0 !important;
+
+}
+.navbar-brand img{
+	height:47px;
+	padding-left:1px;
+
+}
+.navbar-nav{
+	padding:0 35px;
+
+
+}
+.navbar-nav a{
+	color:#49274A;
+	font-weight: 600;
+  font-size:20px;
+
+	font-family:Stencil;
+
+}
+.navbar-nav a:hover{
+	background-color:#b48db0;
+	color:white;
+
+}
+/*---------------nav bar------------------*/
+.banner{
+  position: relative;
+  z-index: 1;
+  min-height: 450px; }
+  .banner .banner_inner {
+position: relative;
+background: url(img/home.jpg) no-repeat center center;
+background-size: cover;
+width: 100%;
+ min-height: 650px;
+z-index: 1; }
+.top-left {
+  position: absolute;
+  top: 17%;
+  left: 10%;
+  font-weight:800;
+  font-size:37px;
+  font-family:stencil;
+  color:#e9dfe8;
+}
+.top-bottom{
+	position:absolute;
+	top:25%;
+	left:14%;
+	 font-weight:800;
+  font-size:37px;
+  font-family:stencil;
+  color:#e9dfe8;
+}
+.button {
+	position: absolute;
+	top:34%;
+	left:20%;
+  background-color:#49274A ;
+  border: none;
+  color: #e9dfe8;
+  padding: 10px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  font-family:Stencil;
+}
+.button1:hover {
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}
+.text1{
+    position: absolute;
+  top: 86%;
+  left: 11%;
+  font-weight:800;
+  font-size:22px;
+  font-family:stencil;
+  color:#49274A;
+}
+ .text2{
+    position: absolute;
+  top: 86%;
+  left: 42%;
+  font-weight:800;
+  font-size:22px;
+  font-family:stencil;
+  color:#49274A;
+}
+.text3{
+    position: absolute;
+  top: 86%;
+  left: 77%;
+  font-weight:800;
+  font-size:22px;
+  font-family:stencil;
+  color:#49274A;
+}
+
+}
+.package-area {
+  padding-bottom: 90px; 
+  color:#f4decb;}
+  @media (max-width: 1199px) {
+    .package-area {
+      padding-bottom: 60px; } }
+
+.single-package {
+  position: relative;
+  padding: 20px 20px 40px;
+  border: 1px solid #eeeeee;
+  margin-bottom: 30px;
+  -webkit-transition: all 0.3s ease 0s;
+  -moz-transition: all 0.3s ease 0s;
+  -o-transition: all 0.3s ease 0s;
+  transition: all 0.3s ease 0s; }
+.single-package .meta-top .fa {
+  margin-right: 5px; }
+.single-package .thumb {
+  overflow: hidden;
+  margin-bottom: 30px; }
+.single-package .thumb img {
+      width: 100%;
+      -webkit-transition: all 0.3s ease 0s;
+      -moz-transition: all 0.3s ease 0s;
+      -o-transition: all 0.3s ease 0s;
+      transition: all 0.3s ease 0s; }
+.single-package h4 {
+    margin-bottom: 12px;
+    font-size: 21px;
+    -webkit-transition: all 0.3s ease 0s;
+    -moz-transition: all 0.3s ease 0s;
+    -o-transition: all 0.3s ease 0s;
+    transition: all 0.3s ease 0s; }
+.single-package a {
+    color: #000000; }
+.single-package .date {
+    color: #fff;
+    padding: 12px 11px;
+    text-align: center;
+    margin-bottom: 0px;
+    display: inline-block;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    font-size: 13px;
+    -webkit-transition: all 0.3s ease 0s;
+    -moz-transition: all 0.3s ease 0s;
+    -o-transition: all 0.3s ease 0s;
+    transition: all 0.3s ease 0s; }
+    .single-package .date span {
+      font-family: "Playfair Display", serif;
+      font-size: 32px; }
+  .single-package .primary-btn {
+    margin-top: 20px; }
+  .single-package:hover {
+    border: 1px solid transparent;
+    box-shadow: 0px 5px 50px rgba(139, 84, 265, 0.4); }
+    .single-package:hover img {
+      transform: scale(1.1); }
+      /*main title*/
+      .main_title {
+  text-align: center;
+  margin-bottom: ;
+    padding-bottom:50px;
+	padding-top:50px; 
+  } }
+  @media (max-width: 1199px) {
+ 
+  
+  .main_title p {
+    display: inline-block;
+    font-weight: 400;
+    margin-top: 0px;
+    text-transform: uppercase;
+    background-image: -webkit-linear-gradient(left, #ff2f8b, #9035f9);
+    background-image: -moz-linear-gradient(left, #ff2f8b, #9035f9);
+    background-image: -ms-linear-gradient(left, #ff2f8b, #9035f9);
+    background-image: -o-linear-gradient(left, #ff2f8b, #9035f9);
+    background-image: linear-gradient(to right, #ff2f8b, #9035f9);
+    color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+    margin: 0px;
+    letter-spacing: 1px; }
+/*  .main_title h1 {
+    font-size: 48px;
+    margin-top: 50px;
+    margin-bottom: 20px; }
+    @media (max-width: 1199px) {
+      .main_title h1 {
+        margin-bottom: 5px;
+        margin-top: 0px; } }
+    @media (max-width: 991px) {
+       }*/
+ /* .main_title .title::before{
+	content:'';
+	background:#7b1798;
+	height:5px;
+	width:200px;
+	margin-right:auto; 
+	margin-left:auto; 
+	display:block;
+	transform: translateY(63px);{
+
+
+  }
+  .main_title .title::after{
+	content:'';
+	background:#7b1798;
+	height:10px;
+	width:50px;
+    margin-right:auto; 
+	margin-left:auto; 
+	margin-bottom:40px;
+	display:block;
+	transform:translateY(0px);
+}
+    }*/
+li{
+  list-style: none;
+}
+}
+/*---------about us--------*/
+#about-us{
+	background:#fff;
+	padding-bottom:50px;
+	padding-top:50px;
+}
+.about-title{
+	font-size:40px;
+	font-weight:600 ;
+	margin-top:8%;
+}
+#about-us ul li{
+	margin: 10px 0px;
+}
+.title::before{
+	content:'';
+	background:#94618E;
+	height:5px;
+	width:200px;
+	margin-right:auto; 
+	margin-left:auto; 
+	display:block;
+	transform: translateY(63px);
+}
+.title::after{
+	content:'';
+	background:#94618E;
+	height:10px;
+	width:50px;
+    margin-right:auto; 
+	margin-left:auto; 
+	margin-bottom:40px;
+
+	display:block;
+	transform: translateY(8px);
+}
+
+#footer{
+  background-image:url(img/footer.jpg);
+  width:100%;
+  height:380px;
+}
+
+}
+.foot{
+  position:relative;
+  background:url(img/footer.png);
+  background-size:cover;
+  width:100%;
+  height:400px;
+
+}
+.team-section{
+  overflow: hidden;
+  text-align:center;
+  background:#e8dbe8;
+  padding:60px;
+
+}
+.team-section h1{
+  text-transform:uppercase;
+  margin-bottom: 60px;
+  color:#49274a;
+  font-size:40px;
+}
+.border{
+  display: block;
+  margin:auto;
+  width:160px;
+  height:5px;
+  background:#49274a;
+  margin-bottom:40px;
+}
+.ps{
+  margin-bottom: 40px;
+
+}
+.ps a{
+  display:inline-block;
+  margin:0 30px;
+  width:160px;
+  height:160px;
+  overflow:hidden;
+  border-radius: 50%;
+
+}
+.ps img{
+  width:100%;
+  filter:grayscale(100%);
+  transition:0.4s all ;
+
+
+}
+.ps a:hover > img{
+  filter:none;
+}
+.sec{
+  width:600px;
+  margin:auto;
+  font-size:20px;
+  color:#49274a;
+  text-align:justify;
+  height:0;
+  overflow: hidden;
+}
+.sec:target{
+  height:auto;
+}
+.name{
+  display:block;
+  margin-bottom:30px;
+  text-align:center;
+  text-transform :uppercase;
+  font-size:22px;
+}
+
+.footer-img{
+width:100%;
+min-height:200px:
+</style>
+<!-- ---------------------NAV BAR ----------------------->
+<section id="nav-bar">
+<nav class="navbar navbar-expand-lg nav-light">
+  <a class="navbar-brand" href="#"><img src="img/logoo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <i class="fa fa-bars"></i>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item ">
+        <a class="nav-link" href="#ghar">HOME </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about-us">ABOUT</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#package">
+         PACKAGES
+        </a>
+ 
+      </li>
+       <li class="nav-item">
+        <a class="nav-link " href="#">BLOG</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">CONTACTS</a>
+      </li>
+    </ul>
+  
+   
+  </div>
+</nav>
+</section>
+<!-------------------------- banner section------------- -->
+
+<section class="banner">
+  <div id="ghar">
+<div class="banner_inner">
+<div class="container">
+
+ <div class="top-left">MOON TRAVELLERS,
+ </div>
+
+ <div class="top-bottom">EXPERIENCE THE BEST</div>
+ <div class="button button1 ">BOOK NOW!</div>
+ <div class="text1">HOTEL BOOKING </div>
+ <div class="text2">DESTINATION BOOKING</div>
+ <div class="text3">FlIGHT BOOKING</div>
+
+ <div class="social_media">
+ <div class="row">
+<!--  <div cass="col-lg-3">
+ <ul><i class="fab fa-facebook"></i></i> -->
+ </li></ul></div></div>
+
+ </div>
+ </div>
+</div>
+ </section>
+
+ <!-- --------------------packages ------------------------------------------------------------------------->
+ <section id="package">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="ol-lg-12">
+          <div class="main_title">
+         
+            <h1 class="title">Famous Trips Packages</h1>
+          
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <div class="single-package">
+            <div class="thumb">
+              <img class="img-fluid" src="img/p1.jpg" alt="">               
+            </div>
+            <p class="date"><span>18</span> <br> December</p>
+            <div class="meta-top d-flex">
+              <p><span class="fa fa-location-arrow"></span> Everest base camp</p>
+              <p class="ml-20"><span class="fa fa-calendar"></span> 5 days 6 nights</p>
+            </div>
+            <h4> MT.EVEREST BASE CAMP</h4>
+            
+            <p>
+              Lorem ipsum dolor sit amet, consecter adipisicing elit, sed do eiusmod tempor incididunt.
+            </p>
+            <a href="#" class="primary-btn">Read More</a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="single-package">
+            <div class="thumb">
+              <img class="img-fluid" src="img/p2.png" alt="">               
+            </div>
+            <p class="date"><span>18</span> <br> December</p>
+            <div class="meta-top d-flex">
+              <p><span class="fa fa-location-arrow"></span> kathmandu-Pokhara tour</p>
+              <p class="ml-20"><span class="fa fa-calendar"></span> 9 days 8 nights</p>
+            </div>
+            <h4>VALLEY TOURS</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consecter adipisicing elit, sed do eiusmod tempor incididunt.
+            </p>
+            <a href="#" class="primary-btn">Read More</a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="single-package">
+            <div class="thumb">
+              <img class="img-fluid" src="img/p3.png" alt="">               
+            </div>
+            <p class="date"><span>18</span> <br> December</p>
+            <div class="meta-top d-flex">
+              <p><span class="fa fa-location-arrow"></span> Kathmandu A day tour</p>
+              <p class="ml-20"><span class="fa fa-calendar"></span>  1 day</p>
+            </div>
+            <h4>A DAY IN KATHMANDU</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consecter adipisicing elit, sed do eiusmod tempor incididunt.
+            </p>
+            <a href="#" class="primary-btn">Read More</a>
+          </div>
+        </div>
+           <div class="col-lg-3 col-md-6">
+          <div class="single-package">
+            <div class="thumb">
+              <img class="img-fluid" src="img/p4.png" alt="">               
+            </div>
+            <p class="date"><span>18</span> <br> December</p>
+            <div class="meta-top d-flex">
+              <p><span class="fa fa-location-arrow"></span> Annapurna base camp</p>
+              <p class="ml-20"><span class="fa fa-calendar"></span>  10 days</p>
+            </div>
+            <h4>ANNAPURNA BASE CAMP</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consecter adipisicing elit, sed do eiusmod tempor incididunt.
+            </p>
+            <a href="#" class="primary-btn">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>  
+  </section>
+  <!----------------------------------- end of package------------------------------------- -->
+  <!-- ----------------------our team------------------------- -->
+  <section>
+  <div class="team-section">
+    <h1 class="title"> Our Team </h1>
+   
+    <div class="ps">
+      <a href="#p1"><img src="img/my.jpg" alt=""></a>
+       <a href="#p2"><img src="img/my.jpg" alt=""></a>
+        <a href="#p3"><img src="img/my.jpg" alt=""></a>
+      </div>
+      <div class="sec" id="p1">
+        <span class="name">Titani Bhuju</span>
+        <span class="border"></span>
+        <p>
+          Lorem ipsum dolor sit amet, consecter adipisicing elit, sed do eiusmod tempor incididunt.
+
+        </p></div>
+         <div class="sec" id="p2">
+        <span class="name">Kanchan Bhuju</span>
+        <span class="border"></span>
+        <p>
+          Lorem ipsum dolor sit amet, consecter adipisicing elit, sed do eiusmod tempor incididunt.
+
+        </p></div>
+         <div class="section" id="p3">
+        <span class="name">Annie Bhuju</span>
+        <span class="border"></span>
+        <p>
+          Lorem ipsum dolor sit amet, consecter adipisicing elit, sed do eiusmod tempor incididunt.
+
+        </p></div>
+</div>
+</section>
+<!-- ---------------------------about us---------------------- -->
+  <section id="about-us">
+  	<div class="container">
+  		<h1 class="title text-center">WHY CHOOSE US?</h1>
+  		<div class="row">
+  			<div class="col-md-6">
+  				<p class="about-title"> Why we're Different?</p>
+  				<ul>
+  					<li>Vitae cumque eius modi expedita</li>
+  					<li>Totam at maxime Accusantium</li>
+  					<li>Distinctio temporibuso</li>
+  				    <li>Vitae cumque eius modi expedita</li>
+  				     <li>Totam at maxime Accusantium</li>
+  				     <li>Distinctio temporibuso</li>
+  			</ul></div>
+
+  				<div class="col-md-6">
+  				<img src="img/travel.jpg" class="img-fluid">
+  				</div>	</div>
+
+     
+</section>
+
+
+
+
+<section id="footer">
+
+
+    <!-- Main Footer Area -->
+    <div class="main-footer bg-img bg-overlay" >
+      <div class="container-fluid">
+        <div class="row" style="padding-top:10%">
+
+          <!-- Single Footer Widget Area -->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="footer-widget ">
+              <a href="#" class="foo-logo d-block mb-30"><img src="img/logo.png" alt="MOONS TRAVELLERS" width:45px height="40px"></a>
+              <p>Lorem ipsum dolor sit amet, consecte stare adipiscing elit. In act honcus risus atiner Pellentesque risus.</p>
+              <div class="contact-info">
+                <p><i class="fa fa-map-pin" aria-hidden="true"></i><span>Bhaktapur,Nepal</span></p>
+                <p><i class="fa fa-envelope" aria-hidden="true"></i><span>kanchanbhuju@gmail.com</span></p>
+                <p><i class="fa fa-phone" aria-hidden="true"></i><span>0166889955</span></p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Single Footer Widget Area -->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="footer-widget mb-80">
+              <h5 class="widget-title">QUICK LINK</h5>
+              <!-- Footer Widget Nav -->
+              <nav class="footer-widget-nav">
+                <ul>
+                  <li><a href="#">Packages</a></li>
+                  <li><a href="#">Blogs</a></li>
+                  <li><a href="#">Bookings</a></li>
+                  <li><a href="#">FAQs</a></li>
+                  <li><a href="#">Return</a></li>
+                  <li><a href="#">Hotels</a></li>
+                 
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <!-- Single Footer Widget Area -->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="footer-widget mb-80">
+              <h5 class="widget-title">RECENT NEWS</h5>
+
+              <!-- Single Recent News Start -->
+              <div class="single-recent-blog d-flex align-items-center">
+                <div class="post-thumbnail">
+                  <img src="img/sack.pngg" alt="">
+                </div>
+                <div class="post-content">
+                  <a href="#" class="post-title">Bhaktapur best city for a tourist</a>
+                  <div class="post-date">18 Aug 2019</div>
+                </div>
+              </div>
+
+              <!-- Single Recent News Start -->
+              <div class="single-recent-blog d-flex align-items-center">
+                <div class="post-thumbnail">
+                  <img src="img/wheat.pngg" alt="">
+                </div>
+                <div class="post-content">
+                  <a href="#" class="post-title">Annapurna base camp</a>
+                  <div class="post-date">18 Aug 2019</div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <!-- Single Footer Widget Area -->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="footer-widget mb-80">
+              <h5 class="widget-title">STAY CONNECTED</h5>
+              <!-- Footer Social Info -->
+              <div class="footer-social-info">
+                <a href="#">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                  <span>Facebook</span>
+                </a>
+                <a href="#">
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+                  <span>Twitter</span>
+                </a>
+                <a href="#">
+                  <i class="fa fa-pinterest" aria-hidden="true"></i>
+                  <span>Pinterest</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+</section>
+
+
+ 
+<script src="smooth-scroll.js"></script>
+<script>
+	var scroll = new SmoothScroll('a[href*="#"]');
+</script>
